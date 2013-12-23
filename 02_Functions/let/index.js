c59e3d05ca76c0a60412d5da5aab3e3bf7a46b79
tcorral <amischol@gmail.com>
2013-12-23 16:55:27 +0100

    Add accessors

---
+++ 02_Functions/let/index.js
@@ -0,0 +1,8 @@
+var test = function()
+{
+  var nCounter = 19;
+  for( let nCounter = 0; nCounter < 10; nCounter++){
+    console.log( 'Inside the loop:', nCounter );
+  }
+  console.log( 'Out of the loop:', nCounter );
+};
